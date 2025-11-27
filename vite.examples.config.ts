e1b73bcdaaa3b9aed5d930da0314ba59114c8571
Flora Hill <imflo.pink@gmail.com>
2025-11-27 13:14:18 +1100

    bweegh

--- vite.examples.config.ts
+++ vite.examples.config.ts
@@ -1,9 +1,14 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   root: 'examples',
+  base: './',
   plugins: [react()],
   server: {
     port: 3001,
